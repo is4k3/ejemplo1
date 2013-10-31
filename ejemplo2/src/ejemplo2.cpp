b08661b7d63c9f47bc057aad34dfa0e90207c161
tincho <margen_87@hotmail.com>
2013-10-31 20:23:45 -0300

    segundos cambios

--- ejemplo2/src/ejemplo2.cpp
+++ ejemplo2/src/ejemplo2.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!vamos a hacer un cambio en el programa!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
